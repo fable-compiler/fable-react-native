64d60fa645c7028a527514597bc98871f7598426
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-12-30 23:06:33 +0100

    Update build script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Fable.React.Native
 
-Fable bindings and helpers for React Native projects
+Fable bindings and helpers for React Native projects.
+
+See [SAFE-Nightwatch](https://github.com/SAFE-Stack/SAFE-Nightwatch) for a Fable RN app sample.
--- build.fsx
+++ build.fsx
@@ -1,324 +1,50 @@
-#r "packages/FAKE/tools/FakeLib.dll"
+// include Fake libs
+#r "./packages/build/FAKE/tools/FakeLib.dll"
 #r "System.IO.Compression.FileSystem"
+#load "paket-files/build/fsharp/FAKE/modules/Octokit/Octokit.fsx"
+#load "paket-files/build/fable-compiler/fake-helpers/Fable.FakeHelpers.fs"
 
-open System
-open System.IO
-open System.Text.RegularExpressions
-open System.Collections.Generic
 open Fake
-open Fake.AssemblyInfoFile
-open Fake.Git
-open Fake.ReleaseNotesHelper
-
-let packages = [
-    "src/Fable.React.Native"
-]
+open Fable.FakeHelpers
+open Octokit
 
 #if MONO
 // prevent incorrect output encoding (e.g. https://github.com/fsharp/FAKE/issues/1196)
 System.Console.OutputEncoding <- System.Text.Encoding.UTF8
 #endif
 
-module Util =
-    open System.Net
-
-    let retryIfFails maxRetries f =
-        let rec loop retriesRemaining =
-            try
-                f ()
-            with _ when retriesRemaining > 0 ->
-                loop (retriesRemaining - 1)
-        loop maxRetries
-
-    let (|RegexReplace|_|) =
-        let cache = new Dictionary<string, Regex>()
-        fun pattern (replacement: string) input ->
-            let regex =
-                match cache.TryGetValue(pattern) with
-                | true, regex -> regex
-                | false, _ ->
-                    let regex = Regex pattern
-                    cache.Add(pattern, regex)
-                    regex
-            let m = regex.Match(input)
-            if m.Success
-            then regex.Replace(input, replacement) |> Some
-            else None
-
-    let join pathParts =
-        Path.Combine(Array.ofSeq pathParts)
-
-    let run workingDir fileName args =
-        printfn "CWD: %s" workingDir
-        let fileName, args =
-            if EnvironmentHelper.isUnix
-            then fileName, args else "cmd", ("/C " + fileName + " " + args)
-        let ok =
-            execProcess (fun info ->
-                info.FileName <- fileName
-                info.WorkingDirectory <- workingDir
-                info.Arguments <- args) TimeSpan.MaxValue
-        if not ok then failwith (sprintf "'%s> %s %s' task failed" workingDir fileName args)
-
-    let start workingDir fileName args =
-        let p = new System.Diagnostics.Process()
-        p.StartInfo.FileName <- fileName
-        p.StartInfo.WorkingDirectory <- workingDir
-        p.StartInfo.Arguments <- args
-        p.Start() |> ignore
-        p
-
-    let runAndReturn workingDir fileName args =
-        printfn "CWD: %s" workingDir
-        let fileName, args =
-            if EnvironmentHelper.isUnix
-            then fileName, args else "cmd", ("/C " + args)
-        ExecProcessAndReturnMessages (fun info ->
-            info.FileName <- fileName
-            info.WorkingDirectory <- workingDir
-            info.Arguments <- args) TimeSpan.MaxValue
-        |> fun p -> p.Messages |> String.concat "\n"
-
-    let downloadArtifact path (url: string) =
-        async {
-            let tempFile = Path.ChangeExtension(Path.GetTempFileName(), ".zip")
-            use client = new WebClient()
-            do! client.AsyncDownloadFile(Uri url, tempFile)
-            FileUtils.mkdir path
-            CleanDir path
-            run path "unzip" (sprintf "-q %s" tempFile)
-            File.Delete tempFile
-        } |> Async.RunSynchronously
-
-    let rmdir dir =
-        if EnvironmentHelper.isUnix
-        then FileUtils.rm_rf dir
-        // Use this in Windows to prevent conflicts with paths too long
-        else run "." "cmd" ("/C rmdir /s /q " + Path.GetFullPath dir)
-
-    let visitFile (visitor: string->string) (fileName : string) =
-        File.ReadAllLines(fileName)
-        |> Array.map (visitor)
-        |> fun lines -> File.WriteAllLines(fileName, lines)
-
-        // This code is supposed to prevent OutOfMemory exceptions but it outputs wrong BOM
-        // use reader = new StreamReader(fileName, encoding)
-        // let tempFileName = Path.GetTempFileName()
-        // use writer = new StreamWriter(tempFileName, false, encoding)
-        // while not reader.EndOfStream do
-        //     reader.ReadLine() |> visitor |> writer.WriteLine
-        // reader.Close()
-        // writer.Close()
-        // File.Delete(fileName)
-        // File.Move(tempFileName, fileName)
-
-    let replaceLines (replacer: string->Match->string option) (reg: Regex) (fileName: string) =
-        fileName |> visitFile (fun line ->
-            let m = reg.Match(line)
-            if not m.Success
-            then line
-            else
-                match replacer line m with
-                | None -> line
-                | Some newLine -> newLine)
-
-    let compileScript symbols outDir fsxPath =
-        let dllFile = Path.ChangeExtension(Path.GetFileName fsxPath, ".dll")
-        let opts = [
-            yield FscHelper.Out (Path.Combine(outDir, dllFile))
-            yield FscHelper.Target FscHelper.TargetType.Library
-            yield! symbols |> List.map FscHelper.Define
-        ]
-        FscHelper.compile opts [fsxPath]
-        |> function 0 -> () | _ -> failwithf "Cannot compile %s" fsxPath
-
-    let normalizeVersion (version: string) =
-        let i = version.IndexOf("-")
-        if i > 0 then version.Substring(0, i) else version
-
-    let assemblyInfo projectDir version extra =
-        let version = normalizeVersion version
-        let asmInfoPath = projectDir </> "AssemblyInfo.fs"
-        (Attribute.Version version)::extra
-        |> CreateFSharpAssemblyInfo asmInfoPath
-
-    type ComparisonResult = Smaller | Same | Bigger
-
-    let foldi f init (xs: 'T seq) =
-        let mutable i = -1
-        (init, xs) ||> Seq.fold (fun state x ->
-            i <- i + 1
-            f i state x)
-
-    let compareVersions (expected: string) (actual: string) =
-        if actual = "*" // Wildcard for custom fable-core builds
-        then Same
-        else
-            let expected = expected.Split('.', '-')
-            let actual = actual.Split('.', '-')
-            (Same, expected) ||> foldi (fun i comp expectedPart ->
-                match comp with
-                | Bigger -> Bigger
-                | Same when actual.Length <= i -> Smaller
-                | Same ->
-                    let actualPart = actual.[i]
-                    match Int32.TryParse(expectedPart), Int32.TryParse(actualPart) with
-                    // TODO: Don't allow bigger for major version?
-                    | (true, expectedPart), (true, actualPart) ->
-                        if actualPart > expectedPart
-                        then Bigger
-                        elif actualPart = expectedPart
-                        then Same
-                        else Smaller
-                    | _ ->
-                        if actualPart = expectedPart
-                        then Same
-                        else Smaller
-                | Smaller -> Smaller)
-
-    let rec findFileUpwards fileName dir =
-        let fullPath = dir </> fileName
-        if File.Exists(fullPath)
-        then fullPath
-        else
-            let parent = Directory.GetParent(dir)
-            if isNull parent then
-                failwithf "Couldn't find %s directory" fileName
-            findFileUpwards fileName parent.FullName
-
-module Npm =
-    let script workingDir script args =
-        sprintf "run %s -- %s" script (String.concat " " args)
-        |> Util.run workingDir "npm"
-
-    let install workingDir modules =
-        let npmInstall () =
-            sprintf "install %s" (String.concat " " modules)
-            |> Util.run workingDir "npm"
-
-        // On windows, retry npm install to avoid bug related to https://github.com/npm/npm/issues/9696
-        Util.retryIfFails (if isWindows then 3 else 0) npmInstall
-
-    let command workingDir command args =
-        sprintf "%s %s" command (String.concat " " args)
-        |> Util.run workingDir "npm"
-
-    let commandAndReturn workingDir command args =
-        sprintf "%s %s" command (String.concat " " args)
-        |> Util.runAndReturn workingDir "npm"
-
-    let getLatestVersion package tag =
-        let package =
-            match tag with
-            | Some tag -> package + "@" + tag
-            | None -> package
-        commandAndReturn "." "show" [package; "version"]
-
-    let updatePackageKeyValue f pkgDir keys =
-        let pkgJson = Path.Combine(pkgDir, "package.json")
-        let reg =
-            String.concat "|" keys
-            |> sprintf "\"(%s)\"\\s*:\\s*\"(.*?)\""
-            |> Regex
-        let lines =
-            File.ReadAllLines pkgJson
-            |> Array.map (fun line ->
-                let m = reg.Match(line)
-                if m.Success then
-                    match f(m.Groups.[1].Value, m.Groups.[2].Value) with
-                    | Some(k,v) -> reg.Replace(line, sprintf "\"%s\": \"%s\"" k v)
-                    | None -> line
-                else line)
-        File.WriteAllLines(pkgJson, lines)
-
-module Node =
-    let run workingDir script args =
-        let args = sprintf "%s %s" script (String.concat " " args)
-        Util.run workingDir "node" args
-
-module Fake =
-    let fakePath = "packages" </> "docs" </> "FAKE" </> "tools" </> "FAKE.exe"
-    let fakeStartInfo script workingDirectory args fsiargs environmentVars =
-        (fun (info: System.Diagnostics.ProcessStartInfo) ->
-            info.FileName <- System.IO.Path.GetFullPath fakePath
-            info.Arguments <- sprintf "%s --fsiargs -d:FAKE %s \"%s\"" args fsiargs script
-            info.WorkingDirectory <- workingDirectory
-            let setVar k v = info.EnvironmentVariables.[k] <- v
-            for (k, v) in environmentVars do setVar k v
-            setVar "MSBuild" msBuildExe
-            setVar "GIT" Git.CommandHelper.gitPath
-            setVar "FSI" fsiPath)
-
-    /// Run the given buildscript with FAKE.exe
-    let executeFAKEWithOutput workingDirectory script fsiargs envArgs =
-        let exitCode =
-            ExecProcessWithLambdas
-                (fakeStartInfo script workingDirectory "" fsiargs envArgs)
-                TimeSpan.MaxValue false ignore ignore
-        System.Threading.Thread.Sleep 1000
-        exitCode
-
-
+let project = "fable-react-native"
 let gitOwner = "fable-compiler"
-let gitHome = "https://github.com/" + gitOwner
 
+let dotnetcliVersion = "2.1.3"
+let mutable dotnetExePath = environVarOrDefault "DOTNET" "dotnet"
 
-let dotnetcliVersion = "2.0.0"
+let CWD = __SOURCE_DIRECTORY__
 
-let mutable dotnetExePath = "dotnet"
-
-
-// Targets
-Target "InstallDotNetCore" (fun _ ->
+// Clean and install dotnet SDK
+Target "Bootstrap" (fun () ->
+    !! "src/bin" ++ "src/obj" |> CleanDirs
     dotnetExePath <- DotNetCli.InstallDotNetSDK dotnetcliVersion
 )
 
+Target "PublishPackages" (fun () ->
+    [ "src/Fable.React.Native.fsproj"]
+    |> publishPackages CWD dotnetExePath
+)
 
-let pushNuget (releaseNotes: ReleaseNotes) (projFiles: string list) =
-    projFiles
-    |> Seq.iter (fun projFile ->
-        let projFile = __SOURCE_DIRECTORY__ </> projFile
-        let projDir = Path.GetDirectoryName(projFile)
-                
-        // Restore dependencies here so they're updated to latest project versions
-        Util.run projDir dotnetExePath "restore"
-        Util.run projDir dotnetExePath (sprintf "pack -c Release /p:Version=%s" releaseNotes.NugetVersion)
-
-        Paket.Push (fun p -> 
-            { p with 
-                WorkingDir = projDir </> "bin" </> "Release" })
+Target "GitHubRelease" (fun () ->
+    let releasePath = CWD </> "RELEASE_NOTES.md"
+    githubRelease releasePath gitOwner project (fun user pw release ->
+        createClient user pw
+        |> createDraft gitOwner project release.NugetVersion
+            (release.SemVer.PreRelease <> None) release.Notes
+        |> releaseDraft
+        |> Async.RunSynchronously
     )
-
-Target "Clean" (fun _ ->
-    !! "**/bin" ++ "**/obj/"
-    |> CleanDirs
 )
 
-Target "Build" (fun () ->
-    for pkg in packages do
-        let projFile = __SOURCE_DIRECTORY__ </> (pkg + ".fsproj")
-        let projDir = Path.GetDirectoryName(projFile)
-        Util.run projDir dotnetExePath "restore"
-        Util.run projDir dotnetExePath "build"
-)
-
-Target "PublishPackages" (fun _ ->
-    for pkg in packages do
-        let projFile = __SOURCE_DIRECTORY__ </> (pkg + ".fsproj")
-        let projDir = Path.GetDirectoryName(projFile)
-        let release =
-            Util.findFileUpwards "RELEASE_NOTES.md" projDir
-            |> ReleaseNotesHelper.LoadReleaseNotes
-        pushNuget release [projFile]
-)
-
-
-Target "Release" DoNothing
-
-"Clean"
-==> "InstallDotNetCore"
+"Bootstrap"
 ==> "PublishPackages"
-==> "Release"
+==> "GitHubRelease"
 
-// Start build
-RunTargetOrDefault "Build"
+RunTargetOrDefault "Bootstrap"
